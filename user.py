7eabf89d8b9fa1270f6e32b831623aac86d4e30e
Otieno100 <brian.masafu@student.moringaschool.com>
2022-04-24 22:09:44 +0300

    created anew instance to generate random password

--- user.py
+++ user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3.9
-from random import random
+from cgi import print_directory
+from hashlib import new
+from random import  randint 
 from password import Password
 
 def create__password(accname,username,phone,password):
@@ -10,21 +12,45 @@ def create__password(accname,username,phone,password):
 def save_password(password):
     password.save_password()
 
-def generate_random_password(password):
-    return Password.random_password(password)
+def generate_random_password(abcdghjkBNHGFUIGY123490):
+    random_password =Password(abcdghjkBNHGFUIGY123490)
+    return random_password
 
 
 def del_password(password):
     password.delete_password() 
 
-def  check_existing_password(username):
+def  check_existing_passwords(username):
      return Password.password_exists(username)  
 
 def display_password():
     return Password.display_passwords() 
 
 
+
+# generating a random pasword
+print("generate a random password for your account")
+def genP(ln)  : 
+    print("generate a random password")
+    login ="abcdfhjkioersHJOOIDIWOWW1234567!@#$%&"
+    pasw1 = ""
+    for i in range(ln):
+        pasw1+=login[randint(0,35)]
+    return pasw1
+print(genP(10))        
+print(genP(10))    
+# print(len(genP))      
+
+
+
+
+
+    
+
+
 def main():
+
+
     print("Habari! Welcome to your password list. What is your name")
     user_name = input()
     print(f"Hello{user_name}.what would you like to do")
@@ -32,11 +58,20 @@ def main():
     print('.\n')
 
 
+
+
+
     while True:
 
-        print ("use this short codes: cp- create a new password,dp-display passwords,cr- create random password,cx- exit the password")
+        print ("use this short codes: cp- create a new password,dp-display passwords,cr- search for random password,cx- exit the password")
 
         short_code = input().lower()
+
+        if short_code == "cr" :
+            print("generate random password")
+            print("-"*10)
+
+
         if short_code == "cp":
             print("new password")
             print("-"*5)
@@ -54,13 +89,21 @@ def main():
             print("password.......")
             password = input()
 
-            
+
+            # save_password(generate_random_password)(abcdghjkBNHGFUIGY123490)
+            # print('.\n')
+            # print(f"password{abcdghjkBNHGFUIGY123490}")
 
 
             save_password(create__password(accname,username,phone,password)) 
             print('.\n')
             print(f"username{username}password{password}created")  
             print('.\n')
+
+
+
+
+
 
 
         elif short_code == "dp" :   
@@ -71,19 +114,41 @@ def main():
                     print(f"{password.account_name}){password.username}....{password.account_password}")
                     print('.\n')
 
-
-        else:
-            print('/n')
-            print("you dont seem to have any login details")
-            print('.\n')
+            else:
+                print('/n')
+                print("you dont seem to have any login details")
+                print('.\n')
 
 
             
 
-        # elif short_code == "cr" :
-        #     print("select a random password")
+        elif short_code == "cr" :
+            print("search for random password")
 
-        #     random_password = input()
+            search_username= input()
+            if check_existing_passwords(search_username) :
+
+                print(f"{search_username.phone}{search_username.account_name}")
+
+
+                print('-'*20)   
+                print(f"phone number......{search_username.phone}")
+                print(f"Email address....{search_username.accout_name}")
+
+
+            else :
+                print("That password does not exist")
+
+
+
+
+
+
+
+
+
+
+
 
 
             
